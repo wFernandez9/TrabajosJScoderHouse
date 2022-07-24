c463012e1e7ca3d215baac376b32fa805f8f7e9e
wFernandez9 <walter1898@hotmail.com>
2022-07-24 13:18:32 -0300

    subida

---
+++ Entregable 1/entregable1.js
@@ -0,0 +1,6 @@
+let numeroIngresado = parseInt(prompt("Ingresar numero a dividor por 2 hasta 10"));
+for (let i = 2; i <= 10; i++) {
+    let resultado = numeroIngresado / i;
+    console.log(numeroIngresado + " / " + i + "=" + resultado);
+}
+
