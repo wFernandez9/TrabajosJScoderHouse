9502c9be9d2cd415f10f4e3d85003e349c749779
wFernandez9 <walter1898@hotmail.com>
2022-07-29 15:19:16 -0300

    subida

---
+++ Entregable 2/js/entregable2.js
@@ -0,0 +1,7 @@
+//Interactua con el usuario
+
+//ingreso cantidad de productos
+let numAdd = parseInt(prompt("Ingrese la cantidad de productos a comprar:(Solo numeros)"));
+
+//precio de productos
+let precioProducto = parseFloat(prompt("Ingrese precio de producto:(Solo numeros)"));
